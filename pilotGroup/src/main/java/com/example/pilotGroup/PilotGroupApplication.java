fb8969c3cb47ea4630489f726035f59eb2777803
lakshek <email2shek@gmail.com>
2025-10-14 18:38:24 -0500

    Project set up

---
+++ pilotGroup/src/main/java/com/example/pilotGroup/PilotGroupApplication.java
@@ -0,0 +1,13 @@
+package com.example.pilotGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotGroupApplication.class, args);
+	}
+
+}
---
+++ pilotGroup/src/test/java/com/example/pilotGroup/PilotGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pilotGroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
